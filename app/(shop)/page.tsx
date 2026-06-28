0b3c1d1ed147584b301dde71d3f6a3da986ec9ec
levanvux <hoanngu512@gmail.com>
2026-06-29 04:18:13 +0700

    feat: add home page

---
+++ app/(shop)/page.tsx
@@ -0,0 +1,11 @@
+import HeroSlider from "@/components/HeroSlider";
+import FeaturedProducts from "@/components/FeaturedProducts";
+
+export default function Home() {
+  return (
+    <main>
+      <HeroSlider />
+      <FeaturedProducts />
+    </main>
+  );
+}
